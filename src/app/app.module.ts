a18631c278dc662e8ef091391a8c1363e9e9045e
Dany PHENGSIAROUN <danyphengsiaroun@danymac.local>
2019-12-09 00:18:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/components/navbar/navbar.component';
+import { SlideComponent } from './home/components/slide/slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
